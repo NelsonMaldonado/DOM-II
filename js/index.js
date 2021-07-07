17bfb8d9fff6b1d7765a4e5407af37d8bc1a62aa
Nelson Maldonado <play2nino@gmail.com>
2021-07-06 22:25:28 -0700

    added event for img class

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const headerGrandParent = document.querySelector('.main-navigation')
+const busImg = document.querySelector('.busImg')
+
+busImg.addEventListener("click", eve =>{
+    console.log(eve)
+})
+
+document.addEventListener("click", e =>{
+    console.log('You clicked on the document')
+})
