8210bb6e8dad6780f7168ba93614beba5f94d193
Nelson Maldonado <play2nino@gmail.com>
2021-08-03 13:29:23 -0700

    first save

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+//1st
+window.onload = function (event){
+    alert('Welcome to the real world, if youre feeling adveturous click ok to continue, otherwise get hacked');
+};
+
+//2nd
